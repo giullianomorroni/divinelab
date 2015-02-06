3fc0b6e78edd8a074ea201d90b0f4f2877d6e785
giullianomorroni <giullianomorroni@gmail.com>
2015-02-06 01:55:29 -0200

    nova atualizacao

--- app/controllers/homeController.go
+++ app/controllers/homeController.go
@@ -16,3 +16,11 @@ func init() {}
 func (this *Home) Index() revel.Result {
 	return this.Render();
 }
+
+func (this *Home) Valores() revel.Result {
+	return this.Render();
+}
+
+func (this *Home) Produtos() revel.Result {
+	return this.Render();
+}
