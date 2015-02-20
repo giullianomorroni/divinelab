b5ac5503302d86ce1dbdee5d6234d3d454eae925
Giulliano Morroni <giullianomorroni@gmail.com>
2015-02-20 10:07:14 -0200

    formulário de contato

--- app/controllers/homeController.go
+++ app/controllers/homeController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	cb "divinelab/app/controllers/base"
 	"github.com/robfig/revel"
+	"net/smtp"
 )
 
 type (
@@ -23,4 +24,21 @@ func (this *Home) Valores() revel.Result {
 
 func (this *Home) Produtos() revel.Result {
 	return this.Render();
+}
+
+func EnviarEmailContato(nome, email, texto string) {
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n";
+	revel.TRACE.Printf("[EmailService.go] Disparo email EnviarEmailContato")
+
+    texto = texto + " MENSAGEM ENVIADA POR " nome +", email para contato: "+ email;
+    msg := []byte(mime + texto)
+
+	auth := smtp.PlainAuth("Divine Slim", "suporte@divineslim.com.br", "ds20142015", "smtp.gmail.com")
+	to := []string{"comercial@divinelab.com.br"}
+	err := smtp.SendMail("smtp.gmail.com:587", auth, "contato@divineslim.com.br", to, msg)
+	if err != nil {
+		revel.TRACE.Printf("Erro no disparo de email %s", err)
+	}
+    this.Flash.Success("Recebemos sua mensagem com sucesso!")
+    return this.Redirect(Home.Index)
 }
